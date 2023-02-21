8d64cf544926c1cea26a79222bbb3f0d62f7d246
celobittar <celobittar@gmail.com>
2023-02-21 09:38:03 -0300

    Adicionar arquivos de projeto.

---
+++ Back/ProEventos.API/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : Controller
+    {
+        public IEnumerable<Evento> _evento = new Evento[]
+            {
+                new Evento(){
+                EventoId = 1,
+                Tema = "Angular e .Net 6",
+                Local = "Santo André",
+                Lote = "1 Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto.png"
+                },
+                new Evento(){
+                EventoId = 2,
+                Tema = "Angular e suas novidades",
+                Local = "São Paulo",
+                Lote = "2 Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto2.png"
+                }
+            };
+
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Fez o Post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put()
+        {
+            return "fez o put";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete()
+        {
+            return "fez o Delete";
+        }
+    }
+}
