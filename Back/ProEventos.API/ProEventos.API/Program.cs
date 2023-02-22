94cc745b3be0e07367c27925f8ff65bea50d3bea
celobittar <celobittar@gmail.com>
2023-02-22 15:38:00 -0300

    Commit Entity Framework Adição de banco de dados

--- Back/ProEventos.API/ProEventos.API/Program.cs
+++ Back/ProEventos.API/ProEventos.API/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using ProEventos.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+IConfiguration Configuration = builder.Configuration;
 
+builder.Services.AddDbContext<DataContext>
+    (
+        context =>  context.UseSqlite(Configuration.GetConnectionString("Default"))
+    );
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
